88117d927ab2a865c64b099d865c57e8877b1ec3
Claudius Tewari <cttdev@users.noreply.github.com>
2021-01-10 22:37:11 -0800

    Update fetch.sh

--- tools/FetchMacOS/fetch.sh
+++ tools/FetchMacOS/fetch.sh
@@ -8,7 +8,7 @@ cd "$SCRIPTDIR" || exit
 # Big Sur uses script 2 for method 1
 if [ "$method" = "method 1" ] && [ "$flavour" == "Big Sur" ] ; then		
 # python3 fetch-macos2.py
-python3 fetch-macos.py # temporary use other big sur download method for now
+python3 fetch-macos3.py # temporary use other big sur download method for now
 # others uses script 1 for method 1
 elif [ "$method" = "method 1" ] ; then
 python3 fetch-macos.py "$@"
@@ -17,7 +17,7 @@ fi
 # Method 2
 # Big Sur uses script 1 for method 2
 if [ "$method" = "method 2" ] && [ "$flavour" == "Big Sur" ] ; then	
-python3 fetch-macos.py "$@"	
+python3 fetch-macos3.py
 # others uses script 2 for method 2
 elif [ "$method" = "method 2" ] ; then
 python3 fetch-macos2.py
